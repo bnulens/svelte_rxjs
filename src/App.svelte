1e5f55ef1d524f1a02129ceb012992d19a0cffb7
bnulens <brechtnulens@gmail.com>
2022-04-25 19:33:34 +0200

    Initial commit - Scss style system scaffolding

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./components/InteractionTracker/InteractionTracker";
+</script>
+
+<main>
+  <div class="container">
+    <div class="app" />
+  </div>
+</main>
+
+<style lang="scss">
+  @import "../scss/main.scss";
+</style>
---
+++ src/components/StateStore/StateStore.svelte
@@ -0,0 +1,7 @@
+<script>
+  let stateStore;
+
+  console.log("Store", stateStore);
+</script>
+
+<div class="app-state-store" bind:this={stateStore} />
