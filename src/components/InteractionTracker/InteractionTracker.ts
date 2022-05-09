a87b9f111f7f1ac45d92d01fb6ba3ee5b1f7f7a0
bnulens <brechtnulens@gmail.com>
2022-05-09 17:46:46 +0200

    InteractionTracker logs all clicks on the page

--- src/components/InteractionTracker/InteractionTracker.ts
+++ src/components/InteractionTracker/InteractionTracker.ts
@@ -2,8 +2,6 @@ import { fromEvent } from "rxjs";
 
 let click$ = fromEvent(document, 'click');
 
-click$.subscribe(console.log)
+// click$.subscribe(console.log)
 
-let interactionTracker;
-
-console.log("Tracker active");
+// console.log("Tracker active");
