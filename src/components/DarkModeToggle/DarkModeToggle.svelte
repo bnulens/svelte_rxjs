8e933a730965c5033278dc2f3e5336c90e2865e5
bnulens <brechtnulens@gmail.com>
2022-05-09 17:44:48 +0200

    Added DarkMode to app root - figure out so kind of app state

---
+++ src/components/DarkModeToggle/DarkModeToggle.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Circle from "../../svgs/Circle.svelte";
+  import Square from "../../svgs/Square.svelte";
+
+  export let toggleView;
+
+  const toggleViewMode = () => (toggleView = !toggleView);
+</script>
+
+<button class="toggle-test" on:click={toggleViewMode}>
+  <div class="toggle-test-inner">
+    {#if toggleView === false}
+      <svelte:component this={Circle} />
+    {:else if toggleView === true}
+      <svelte:component this={Square} />
+    {/if}
+  </div>
+</button>
+
+<style lang="scss">
+  $btn-width: 40px;
+  $btn-height: 40px;
+  $btn-font-size: 12px;
+
+  .toggle-test {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: $btn-height;
+    width: $btn-width;
+    padding: 12px 6px;
+    font-size: $btn-font-size;
+    font-weight: 700;
+    transition: all 0.4s ease;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:hover {
+      height: calc($btn-height + 2%);
+      width: calc($btn-width + 1%);
+    }
+
+    .toggle-test-inner {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  :global(svg) {
+    display: block;
+    /* max-width: 100px; */
+    /* max-height: 100px; */
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import App from "./App.svelte";
 
+import type { IAppStore } from './interfaces/AppStore.interface'
+
+const store: IAppStore = {
+  darkMode: false,
+}
+
 const app = new App({
   target: document.body,
-  // props: {
-  // 	name: 'world'
-  // }
+  props: {
+    title: "Title injected into App root",
+  	store: store
+  }
 });
 
 export default app;
