db52c63cb0d457d663db4d3e2ae951bf1e54a552
bnulens <brechtnulens@gmail.com>
2022-05-09 17:45:42 +0200

    Using DarkMode toggle - added Disclaimer as a messaging component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
+  import DarkModeToggle from "./components/DarkModeToggle/DarkModeToggle.svelte";
+  import Disclaimer from "./components/Disclaimer/Disclaimer.svelte";
+
   import "./components/InteractionTracker/InteractionTracker";
+
+  import type { IAppStore } from "./interfaces/AppStore.interface";
+
+  export let title: string;
+  export let store: IAppStore;
+
+  $: viewMode = store.darkMode;
 </script>
 
 <main>
   <div class="container">
-    <div class="app" />
+    <Disclaimer bind:store />
+    <div class="app">
+      <h1 class="title-main">{title}</h1>
+      <DarkModeToggle bind:toggleView={store.darkMode} />
+      <span>{viewMode ? "dark" : "light"}</span>
+    </div>
   </div>
 </main>
 
--- src/components/Disclaimer/Disclaimer.svelte
+++ src/components/Disclaimer/Disclaimer.svelte
@@ -1,4 +1,83 @@
 <script>
+  export let store;
+
+  const closeDisclaimer = () => {
+    console.log("close");
+  };
 </script>
 
-<div class="disclaimer-wrapper" />
+<div class="disclaimer-wrapper">
+  <div class="disclaimer-inner">
+    <div class="disclaimer-inner-close">
+      <button class="disclaimer-inner-btn-close" on:click={closeDisclaimer}>
+        <span />
+        <span />
+      </button>
+    </div>
+    <h3>&lt;Disclaimer&sol;&gt;</h3>
+    <p>Generic text</p>
+  </div>
+</div>
+
+<style lang="scss">
+  .disclaimer-wrapper {
+    position: absolute;
+    display: block;
+    bottom: 0;
+    left: 50%;
+    width: 90%;
+    max-width: 1080px;
+    height: 100px;
+    margin: 0 auto;
+    color: white;
+    transform: translate(-50%, 0%);
+    border: 1px solid red;
+    border-radius: 12px;
+
+    .disclaimer-inner {
+      position: relative;
+      background-color: hsl(349, 100%, 50%);
+      height: 100%;
+      padding: 12px 16px;
+      border-radius: 12px;
+
+      .disclaimer-inner-close {
+        position: absolute;
+        top: 50%;
+        right: 0;
+        transform: translateY(-50%);
+
+        .disclaimer-inner-btn-close {
+          position: relative;
+          background-color: transparent;
+          width: 40px;
+          height: 40px;
+          border: 1px solid transparent;
+          border-radius: 50%;
+          cursor: pointer;
+
+          span:nth-child(1) {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            display: block;
+            width: 5px;
+            height: 20px;
+            background-color: white;
+            transform: translate(-50%, -50%) rotate(45deg);
+          }
+          span:nth-child(2) {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            display: block;
+            width: 5px;
+            height: 20px;
+            background-color: white;
+            transform: translate(-50%, -50%) rotate(135deg);
+          }
+        }
+      }
+    }
+  }
+</style>
